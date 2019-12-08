3fb1e21e8708d0c01304ed0a1b0f661d2cd5ceda
Mustafa ER <mustafaer@protonmail.com>
2019-12-08 22:18:07 +0300

    project general structure

--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,156 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ResistanceCalculator
 {
     public partial class frm_resstanceCalculator : Form
     {
+        string[,] resBandValueArray = new string[,] { { "Siyah", "Kahverengi", "Kırmızı", "Turuncu", "Sarı", "Yeşil", "Mavi", "Mor", "Gri", "Beyaz" }, { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" } };
+        string[,] multiplierBandValueArray = new string[,] { { "Siyah", "Kahverengi", "Kırmızı", "Turuncu", "Sarı", "Yeşil", "Mavi", "Mor", "Altın", "Gümüş" }, { " x 1", " x 10", " x 100", " x 10^3", " x 10^4", " x 10^5", " x 10^6", " x 10^7", " x 0.1", " x 0.01" } };
+        string[,] toleranceBandValueArray = new string[,] { { "Kahverengi", "Kırmızı", "Yeşil", "Mavi", "Mor", "Gri", "Altın", "Gümüş", "Renksiz" }, { "± %1", "± %2", "± %0.5", "± %0.25", "± %0.10", "± %0.05", "± %5", "± %10", "± %20" } };
+        string[,] temperacureBandValueArray = new string[,] { { "Kahverengi", "Kırmızı", "Turuncu", "Sarı" }, { "100 ppm", "50 ppm", "15 ppm", "25 ppm"} };
+        string[] resistanceValueArray = new string[4] {"","","","" };
+        string resistanceTolerance = "";
+        string resistanceTeperature = "";
+
         public frm_resstanceCalculator()
         {
             InitializeComponent();
+
+        }
+
+        private void btn_fourBandRes_Click(object sender, EventArgs e)
+        {
+            pnl_color3Band.Visible = false;
+            pnl_temperatureBand.Visible = false;
+        }
+
+        private void btn_fiveBandRes_Click(object sender, EventArgs e)
+        {
+            pnl_color3Band.Visible = true;
+            pnl_temperatureBand.Visible = false;
+        }
+
+        private void btn_sixBandRes_Click(object sender, EventArgs e)
+        {
+            pnl_color3Band.Visible = true;
+            pnl_temperatureBand.Visible = true;
+        }
+
+        private void frm_resstanceCalculator_Load(object sender, EventArgs e)
+        {
+            btn_fourBandRes.PerformClick();
+
+            int loopCount = 0;
+            int arrayIndex = 0;
+            Array[] newArrayList= new Array[] {resBandValueArray, multiplierBandValueArray, toleranceBandValueArray, temperacureBandValueArray };
+            for (int i = 0; i <= 5; i++)
+            {
+                int startIndex = 0;
+                arrayIndex++;
+                if (loopCount == 0 || loopCount ==1 || loopCount == 2)
+                {
+                    arrayIndex = 0;
+                    if (loopCount == 2)
+                    {
+                        startIndex = 1;
+                    }
+                }
+
+                for (int j = startIndex; j < newArrayList[arrayIndex].Length / 2; j++)
+                {
+                    switch (loopCount)
+                    {
+                        case 0:
+                            cbx_resBand1.Items.Add(resBandValueArray[0, j]);
+                            break;
+                        case 1:
+                            cbx_resBand2.Items.Add(resBandValueArray[0, j]);
+                            break;
+                        case 2:
+                            cbx_resBand3.Items.Add(resBandValueArray[0, j]);
+                            startIndex = 0;
+                            break;
+                        case 3:
+                            cbx_multiplierBand.Items.Add(multiplierBandValueArray[0, j]);
+                            break;
+                        case 4:
+                            cbx_toleranceBand.Items.Add(toleranceBandValueArray[0, j]);
+                            break;
+                        case 5:
+                            cbx_temperatureBand.Items.Add(temperacureBandValueArray[0, j]);
+                            break;
+                    }
+                }
+                loopCount++;
+            }
+        }
+
+        private void cbx_resBand1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = Char.IsLetterOrDigit(e.KeyChar) || Char.IsSymbol(e.KeyChar) || Char.IsPunctuation(e.KeyChar) || Char.IsWhiteSpace(e.KeyChar) || Char.IsControl(e.KeyChar) || Char.IsNumber(e.KeyChar);
+        }
+
+        private void cbx_resBand2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = Char.IsLetterOrDigit(e.KeyChar) || Char.IsSymbol(e.KeyChar) || Char.IsPunctuation(e.KeyChar) || Char.IsWhiteSpace(e.KeyChar) || Char.IsControl(e.KeyChar) || Char.IsNumber(e.KeyChar);
+        }
+
+        private void cbx_resBand3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = Char.IsLetterOrDigit(e.KeyChar) || Char.IsSymbol(e.KeyChar) || Char.IsPunctuation(e.KeyChar) || Char.IsWhiteSpace(e.KeyChar) || Char.IsControl(e.KeyChar) || Char.IsNumber(e.KeyChar);
+        }
+
+        private void cbx_multiplierBand_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = Char.IsLetterOrDigit(e.KeyChar) || Char.IsSymbol(e.KeyChar) || Char.IsPunctuation(e.KeyChar) || Char.IsWhiteSpace(e.KeyChar) || Char.IsControl(e.KeyChar) || Char.IsNumber(e.KeyChar);
+        }
+
+        private void cbx_toleranceBand_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = Char.IsLetterOrDigit(e.KeyChar) || Char.IsSymbol(e.KeyChar) || Char.IsPunctuation(e.KeyChar) || Char.IsWhiteSpace(e.KeyChar) || Char.IsControl(e.KeyChar) || Char.IsNumber(e.KeyChar);
+        }
+
+        private void cbx_temperatureBand_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = Char.IsLetterOrDigit(e.KeyChar) || Char.IsSymbol(e.KeyChar) || Char.IsPunctuation(e.KeyChar) || Char.IsWhiteSpace(e.KeyChar) || Char.IsControl(e.KeyChar) || Char.IsNumber(e.KeyChar);
+        }
+
+        private void cbx_resBand1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            resistanceValueArray[0] = resBandValueArray[1, cbx_resBand1.SelectedIndex];
+        }
+
+        private void cbx_resBand2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            resistanceValueArray[1] = resBandValueArray[1, cbx_resBand2.SelectedIndex];
+        }
+
+        private void cbx_resBand3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            resistanceValueArray[2] = resBandValueArray[1, cbx_resBand3.SelectedIndex];
+        }
+
+        private void cbx_multiplierBand_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            resistanceValueArray[3] = multiplierBandValueArray[1, cbx_multiplierBand.SelectedIndex];
+        }
+
+        private void btn_calculateResistance_Click(object sender, EventArgs e)
+        {
+            lbl_resistanceResult.Text = resistanceValueArray[0]+ resistanceValueArray[1]+ resistanceValueArray[2]+ resistanceValueArray[3];
+            lbl_toleranceResult.Text = resistanceTolerance;
+            lbl_temperatureResult.Text = resistanceTeperature;
+        }
+
+        private void cbx_toleranceBand_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            resistanceTolerance = toleranceBandValueArray[1, cbx_toleranceBand.SelectedIndex];
+        }
+
+        private void cbx_temperatureBand_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            resistanceTeperature = temperacureBandValueArray[1, cbx_temperatureBand.SelectedIndex];
         }
     }
 }
--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ResistanceCalculator.Properties
-{
-
-
+namespace ResistanceCalculator.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -19,52 +19,75 @@ namespace ResistanceCalculator.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ResistanceCalculator.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap fa41d8476ebb5d898bd32f305883e1ce {
+            get {
+                object obj = ResourceManager.GetObject("fa41d8476ebb5d898bd32f305883e1ce", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap five {
+            get {
+                object obj = ResourceManager.GetObject("five", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Resistor_Color_Code_Manufacturers_typically_use_a_color_band_system_known_as_the_resistor_color_code_ {
+            get {
+                object obj = ResourceManager.GetObject("Resistor+Color+Code+Manufacturers+typically+use+a+color+band+system+known+as+the+" +
+                        "resistor+color+code.", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
